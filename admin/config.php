bc7f82e94634cb410b40139c54a479cd8e6e69d1
lennon <lennon.chan@apptisan.hk>
2016-11-20 18:42:02 +0800

    opencart init

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/oc2302/admin/');
+define('HTTP_CATALOG', 'http://localhost/oc2302/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/oc2302/admin/');
+define('HTTPS_CATALOG', 'http://localhost/oc2302/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Program Files/phpStudy/WWW/oc2302/admin/');
+define('DIR_SYSTEM', 'D:/Program Files/phpStudy/WWW/oc2302/system/');
+define('DIR_IMAGE', 'D:/Program Files/phpStudy/WWW/oc2302/image/');
+define('DIR_LANGUAGE', 'D:/Program Files/phpStudy/WWW/oc2302/admin/language/');
+define('DIR_TEMPLATE', 'D:/Program Files/phpStudy/WWW/oc2302/admin/view/template/');
+define('DIR_CONFIG', 'D:/Program Files/phpStudy/WWW/oc2302/system/config/');
+define('DIR_CACHE', 'D:/Program Files/phpStudy/WWW/oc2302/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Program Files/phpStudy/WWW/oc2302/system/storage/download/');
+define('DIR_LOGS', 'D:/Program Files/phpStudy/WWW/oc2302/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Program Files/phpStudy/WWW/oc2302/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Program Files/phpStudy/WWW/oc2302/system/storage/upload/');
+define('DIR_CATALOG', 'D:/Program Files/phpStudy/WWW/oc2302/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'oc2302');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/oc2302/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/oc2302/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Program Files/phpStudy/WWW/oc2302/catalog/');
+define('DIR_SYSTEM', 'D:/Program Files/phpStudy/WWW/oc2302/system/');
+define('DIR_IMAGE', 'D:/Program Files/phpStudy/WWW/oc2302/image/');
+define('DIR_LANGUAGE', 'D:/Program Files/phpStudy/WWW/oc2302/catalog/language/');
+define('DIR_TEMPLATE', 'D:/Program Files/phpStudy/WWW/oc2302/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/Program Files/phpStudy/WWW/oc2302/system/config/');
+define('DIR_CACHE', 'D:/Program Files/phpStudy/WWW/oc2302/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Program Files/phpStudy/WWW/oc2302/system/storage/download/');
+define('DIR_LOGS', 'D:/Program Files/phpStudy/WWW/oc2302/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Program Files/phpStudy/WWW/oc2302/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Program Files/phpStudy/WWW/oc2302/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'oc2302');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
